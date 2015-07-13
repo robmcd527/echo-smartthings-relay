e73ffeee3eeaeaffc23590d73b039efab5ca4715
robmcd527 <robmcd@gmail.com>
2015-07-13 03:42:50 +0000

    Adding support for device groups

--- index.js
+++ index.js
@@ -10,9 +10,11 @@
 var fs = require('fs');
 var AWS = require('aws-sdk');
 var kms = new AWS.KMS();
+var dynamoDB = new AWS.DynamoDB();
 var request = require('request');
 var FuzzySet = require('fuzzyset.js');
 var async = require('async');
+var uuid = require('uuid');
 
 // SmartThings SmartApp connection info
 var smartthingsBaseURL = 'https://graph.api.smartthings.com/api/smartapps/installations';
@@ -94,6 +96,10 @@ function onIntent(intentRequest, session, callback) {
     // Dispatch to your skill's intent handlers
     if ("ToggleSwitch" === intentName) {
         setSwitchStatus(intent, session, callback);
+    } else if ("CreateGroup" === intentName) {
+        createGroup(intent, session, callback);
+    } else if ("AddDeviceToGroup" === intentName) {
+        addDeviceToGroup(intent, session, callback);
     } else {
         throw "Invalid intent";
     }
@@ -127,10 +133,186 @@ function getWelcomeResponse(callback) {
 }
 
 /**
+ * Create a new SmartThings device group
+ */
+function createGroup(intent, session, alexaCB) {
+    var cardTitle = "SmartThings Group Creation";
+    var groupNameSlot = intent.slots.Group;
+    var repromptText = "";
+    var sessionAttributes = {};
+    var shouldEndSession = true;
+    var speechOutput = '';
+    
+    console.log("Detected intent to create a new device group: " + groupNameSlot.value);
+    
+    async.waterfall(
+        [
+            // FIrst grab the existing list of groups
+            getSmartThingsGroups,
+            // Next see if we have a conflict
+            function(data, callback) {
+                console.log("Checking to see if " + groupNameSlot.value + " or something similar is already a group " +
+                    "name");
+                var group = findClosestMatch(data.Items, groupNameSlot.value, 0.9, function(il) { return il.GroupName.S; });
+                
+                if (group != null) {
+                    callback('WARN', "A group called " + groupNameSlot.value + " already exists!");
+                }
+                else {
+                    callback(null, groupNameSlot.value);
+                }
+            },
+            // Go ahead and create the new group
+            function(groupName, callback) {
+                console.log("Proceeding to create new group: " + groupName);
+                
+                var groupId = uuid.v4();
+                console.log("Created UUID " + groupId);
+                
+                var params = {
+                  'Item': {
+                      'GroupID': {'S': groupId},
+                      'GroupName': {'S': groupName},
+                  },
+                  'TableName': 'SmartThingsGroups'
+                };
+                
+                dynamoDB.putItem(params, function(err, data) {
+                   if (err) {
+                       console.log("Error creating new group in DYnamoDB: " + err);
+                       speechOutput = "Sorry, I was unable to create a new group called " + groupName + " in dynamo";
+                   }
+                   else {
+                       console.log("Successfully created new group " + groupName);
+                       speechOutput = "I have created a new Smart Things group called " + groupName + ". You can now " +
+                            "add devices to it.";
+                   }
+                   
+                   alexaCB(sessionAttributes,
+                            buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+                });
+            }
+        ],
+        function(err, data) {
+            if (err) {
+                console.log(err + ": " + data);
+                speechOutput = data;
+            }
+            else {
+                console.log("Successfully finished processing - Initiating callback to Alexa.");
+                speechOutput = data;
+            }
+            alexaCB(sessionAttributes,
+                buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+        }
+    );
+}
+
+
+/**
+ * Add a device to a group
+ */
+function addDeviceToGroup(intent, session, alexaCB) {
+    var cardTitle = "SmartThings Add Device to Group";
+    var deviceNameSlot = intent.slots.Device;
+    var groupNameSlot = intent.slots.Group;
+    var repromptText = "";
+    var sessionAttributes = {};
+    var shouldEndSession = true;
+    var speechOutput = '';
+    
+    console.log("Detected intent to add device " + deviceNameSlot.value + " to group " + groupNameSlot.value);
+    
+    async.waterfall(
+        [
+            // FIrst grab the existing list of groups
+            getSmartThingsGroups,
+            // Next see if we have a conflict
+            function(data, callback) {
+                console.log("Searching for group called " + groupNameSlot.value + "...");
+                var group = findClosestMatch(data.Items, groupNameSlot.value, 0.9, 
+                    function(il) { return il.GroupName.S; });
+                
+                if (group == null) {
+                    callback('WARN', "Sorry, I can't find a group called " + groupNameSlot.value);
+                }
+                else {
+                    console.log(group);
+                    console.log("Found group " + group.GroupName.S + " with ID " + group.GroupID.S);
+                    callback(null, group.GroupID.S);
+                }
+            },
+            // Grab the oAuth token
+            function(groupId, callback) {
+                getSmartThingsToken(function(err, data) {
+                    callback(err, groupId, data);
+                });
+            },
+            // Get the list of devices
+            function(groupId, oauthToken, callback) {
+                getSwitchInformation(oauthToken, function(err, data) {
+                    callback(err, groupId, data);
+                });
+            },
+            // Figure out which device we want to add
+            function(groupId, deviceList, callback) {
+                var device = findClosestMatch(deviceList, deviceNameSlot.value, 0.7, function(il) { return il.name; });
+                
+                // Null return value means none of the devices we loaded were similar enough
+                if (null == device) {
+                    callback('WARN', "Unable to locate device with a name similar to " + deviceNameSlot.value + 
+                        ". Please try again.");
+                    return;
+                }
+                
+                callback(null, groupId, device.id);
+                
+            },
+            // Go ahead and add the device to the group
+            function(groupId, deviceId, callback) {
+                console.log("Proceeding to add " + deviceId + " to group id " + groupId);
+                
+                var params = {
+                  'Item': {
+                      'DeviceID': {'S': deviceId},
+                      'GroupID': {'S': groupId},
+                  },
+                  'TableName': 'SmartThingsDeviceMappings'
+                };
+                
+                dynamoDB.putItem(params, function(err, data) {
+                   if (err) {
+                       console.log("Error creating new device mapping in dynamo: " + err);
+                       callback('ERROR', "Sorry, I was unable to add your device to dynamo.");
+                   }
+                   else {
+                       console.log("Successfully created new group.");
+                       callback(null, "I have successfully added your device.");;
+                   }
+                });
+            }
+        ],
+        function(err, data) {
+            if (err) {
+                console.log(err + ": " + data);
+                speechOutput = data;
+            }
+            else {
+                console.log("Successfully finished processing - Initiating callback to Alexa.");
+                speechOutput = data;
+            }
+            alexaCB(sessionAttributes,
+                buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+        }
+    );
+}
+
+
+/**
  * Turn a switch on or off
  */
 function setSwitchStatus(intent, session, alexaCB) {
-    var cardTitle = intent.name;
+    var cardTitle = "SmartThings Action";
     var actionSlot = intent.slots.Action;
     var switchNameSlot = intent.slots.Switch;
     var repromptText = "";
@@ -144,7 +326,7 @@ function setSwitchStatus(intent, session, alexaCB) {
     // Use async to tame some of the nested callbacks
     async.waterfall(
         [
-            // First do some basic error checking and grab the oauth token
+            // First do some basic error checking
             function(callback) {
                 if (! (actionSlot.value === 'on' || actionSlot.value === 'off') ) {
                     callback(true, "Sorry, I can only turn devices on or off.  It sounds like you asked me to turn " + 
@@ -157,32 +339,20 @@ function setSwitchStatus(intent, session, alexaCB) {
                     return;
                 }
                 
-                console.log("Reading encrypted smartthings API token from local file " + encryptedTokenPath);
-                var encryptedToken = fs.readFileSync(encryptedTokenPath);
-        
-                var params = {
-                    CiphertextBlob: encryptedToken
-                };
-                
-                console.log("Decrypting token using KMS...");
-                kms.decrypt(params, function(err, data) {
-                    if (err) {
-                        console.log("Error decrypting token: " + data);
-                        callback('ERROR', "Unable to decrypt token using KMS");
-                    }
-                    else {
-                        var smartThingsToken = data['Plaintext'].toString();
-                        console.log("Successfully decrypted smartthings API token.");
-                        callback(null, smartThingsToken);
-                    }
-                })
+                callback(null);
             },
+            // Grab the oAuth token
+            getSmartThingsToken,
             // Go grab the list of devices and pass that down
-            getSwitchInformation,
+            function(oauthToken, callback) {
+                getSwitchInformation(oauthToken, function(err, data) {
+                    callback(err, oauthToken, data);
+                });
+            },
             // Try to figure out which device we want to work with
             function(oauthToken, data, callback) {
                 // Load the device that is the closest match to the requested name
-                var device = findClosestMatchingDevice(data, switchNameSlot.value);
+                var device = findClosestMatch(data, switchNameSlot.value, 0.5, function(il) { return il.name; });
                     
                 // Null return value means none of the devices we loaded were similar enough
                 if (null == device) {
@@ -192,7 +362,7 @@ function setSwitchStatus(intent, session, alexaCB) {
                 }
                     
                 // Don't try to turn on a device that is already on
-                if (device.value === actionSlot.value) {
+                if (device.name === actionSlot.value) {
                     callback('INFO', "Looks like the " + device.name + " is already " + actionSlot.value);
                     return;
                 }
@@ -217,32 +387,33 @@ function setSwitchStatus(intent, session, alexaCB) {
 }
 
 /**
- * This method takes the structured list of devices retrieved from SmartThings
- * and tries to identify which one we want to interact with.  It first looks for
- * any exact matches and then tries to do a fuzzy string match in case Echo 
- * interpreted something a little off.  Returns null if we don't find a good 
- * match.
+ * This method takes the structured list of data and tries to find the item
+ * that matches most clostly.  It first looks for any exact matches and then 
+ * tries to do a fuzzy string match in case Echo interpreted something a 
+ * little off.  Returns null if we don't find a good match.
  * 
  * Reference: https://www.npmjs.com/package/fuzzyset.js
  */
-function findClosestMatchingDevice(deviceList, deviceName) {
-    console.log("Attempting to find device matching " + deviceName + "...");
+function findClosestMatch(sourceList, target, fuzzyThreshold, accessor) {
+    console.log("Attempting to find source string matching " + target + "...");
     
-    var nameList = [];
-    var fuzzyThreshold = 0.5;
+    accessor = (accessor == null) ? function(li) { return li; } : accessor;
     
-    for (var i = 0; i < deviceList.length; i++) {
-        nameList.push(deviceList[i].name);
-        if (deviceList[i].name.toLocaleLowerCase() === deviceName.toLocaleLowerCase()) {
-            console.log("Returning exact match of " + deviceList[i].name);
-            return deviceList[i];
+    var collapsedList = [];
+    
+    for (var i = 0; i < sourceList.length; i++) {
+        var item = accessor(sourceList[i]);
+        collapsedList.push(item);
+        if (item.toLocaleLowerCase() === target.toLocaleLowerCase()) {
+            console.log("Returning exact match of " + item);
+            return sourceList[i];
         }
     }
     
     console.log("No exact match found - proceeding to fuzzy matching...");
     
-    var fuzzy = FuzzySet(nameList);
-    var matchData = fuzzy.get(deviceName);
+    var fuzzy = FuzzySet(collapsedList);
+    var matchData = fuzzy.get(target);
     
     if (! matchData) {
         console.log("FUzzy match failed!");
@@ -255,10 +426,10 @@ function findClosestMatchingDevice(deviceList, deviceName) {
     var matchScore = matchData[0][0];
     
     if (matchScore > fuzzyThreshold) {
-        for (var i = 0; i < deviceList.length; i++) {
-            if (deviceList[i].name === matchName) {
+        for (var i = 0; i < sourceList.length; i++) {
+            if (accessor(sourceList[i]) === matchName) {
                 console.log("Returning fuzzy match of " + matchName + " that had score of " + matchScore);
-                return deviceList[i];
+                return sourceList[i];
             }
         }
     }
@@ -268,6 +439,52 @@ function findClosestMatchingDevice(deviceList, deviceName) {
     }
     
     return null;
+}
+
+
+/**
+ * Get the list of groups from Dynamo
+ */
+function getSmartThingsGroups(callback) {
+    var params = {
+        'TableName': 'SmartThingsGroups'
+    };
+    
+    dynamoDB.scan(params, function(err, data) {
+       if (err) {
+           console.log("Error loading groups from DYnamoDB: " + err);
+           callback('ERROR', "Sorry, I was unable to load existing group list from Dynamo.");
+       }
+       else {
+           console.log("Successfully loaded groups from DynamoDB.");
+           callback(null, data);
+       }
+    });
+}
+
+/**
+ * Gets the SmartThings oAuth token from KMS
+ */
+function getSmartThingsToken(callback) {
+    console.log("Reading encrypted smartthings API token from local file " + encryptedTokenPath);
+    var encryptedToken = fs.readFileSync(encryptedTokenPath);
+
+    var params = {
+        CiphertextBlob: encryptedToken
+    };
+    
+    console.log("Decrypting token using KMS...");
+    kms.decrypt(params, function(err, data) {
+        if (err) {
+            console.log("Error decrypting token: " + data);
+            callback('ERROR', "Unable to decrypt token using KMS");
+        }
+        else {
+            var smartThingsToken = data['Plaintext'].toString();
+            console.log("Successfully decrypted smartthings API token.");
+            callback(null, smartThingsToken);
+        }
+    });
 }
 
 
@@ -290,7 +507,7 @@ function getSwitchInformation(oauthToken, callback) {
             }
             else {
                 console.log("Successfully loaded switch details from web service: " + body);
-                callback(null, oauthToken, JSON.parse(body));
+                callback(null, JSON.parse(body));
             }
         }
     );
@@ -333,8 +550,8 @@ function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
         },
         card: {
             type: "Simple",
-            title: "SessionSpeechlet - " + title,
-            content: "SessionSpeechlet - " + output
+            title: title,
+            content: output
         },
         reprompt: {
             outputSpeech: {
